300eeb82e67a7c63273daee41536c6070284bed5
sdfz27 <sdfz27@gmail.com>
2026-03-22 17:18:39 -0400

    fix: remove lint script wire jest-dom

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from "./App";
 describe("App", () => {
   it("renders root title", () => {
     render(<App />);
-    expect(screen.getByRole("heading", { name: /pinot/i })).toBeDefined();
+    expect(screen.getByRole("heading", { name: /pinot/i })).toBeInTheDocument();
   });
 });
